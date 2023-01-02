b011982c1df2e85bc94a9b8458b899260b7f2485
Bright Abety <bright@Brights-MacBook-Pro.local>
2023-01-01 22:48:52 -0500

    dashboaod updates

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 ## Server-Side APIs Challenge: Weather Dashboard
-  In this week's challenge, we using Third-party APIs to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+  In this week's challenge, we will be using Third-party APIs to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS. This third-party APIS will allow developers to access to their data and functonality by making requests with specific parameters to a URL.
 
 Technologies used: HTML, CSS, JavaScript, JQuery
 
 ## Links:
-Website: 
+Check out my application: 
 
-GitHub: https://github.com/kagebright/weatherdashboard
+### GitHub: https://github.com/kagebright/weatherdashboard
+### Website: 
+
+### Screenshots: 
--- dashboard.js
+++ dashboard.js
@@ -1,18 +1,39 @@
-$(function) () {
-//declaring all the variables
-    const cites = $(".cities");
-    const temperature = $(".temp");
-    const
+$(document).ready(function(){
+    //apiKey from OpenWeatherMap
+    let apiKey = "dc9a13c8fc4afc874882b2c3a86a4300",
+
+    //declaring the variables
+    const listofCities = $(".listOfCities");
+    const mainCity = $("h3.searchedCity");
+    const temp = $(".temp");
+    const humidity = $(".humidity");
+    const windSpeed = $(".windSpeed");
+    const uvIndex = $(".uvIndex");
 
 
-fetch("http://api.openweathermap.org/geo/1.0/direct?q={lowell},{25},{country code}&limit={limit}&appid={dc9a13c8fc4afc874882b2c3a86a4300}")
-    .then(function (response) {
-        if (response.ok) {
-            response.json()
-            .then(function(data)) {
-                displayIssues(data);
-            }
-        }
-    });
+});
 
+fetch(
+    "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "units=metric&appid=" + apiKey);
+    .then((response) => response.JSON());
+    .then((data) => this.displayWeather(data));
+    
+
+displayWeather= function(data) {
+    document.querySelector(".city").innerText = "Weather in " + name; 
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°F";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "windSpeed: " + speed + "mph";
 };
+
+
+document.querySelector(".search-button").addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
