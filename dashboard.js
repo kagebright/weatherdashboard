6b6fc4aa50e2ce0aaefab90c36887af463954b61
Bright Abety <bright@Brights-MacBook-Pro.local>
2023-01-02 23:13:48 -0500

    final

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function() {
     //apiKey from OpenWeatherMap
-    let apiKey = "dc9a13c8fc4afc874882b2c3a86a4300",
-
+    const apiKey = "dc9a13c8fc4afc874882b2c3a86a4300";
+  
     //declaring the variables
     const listofCities = $(".listOfCities");
     const mainCity = $("h3.searchedCity");
@@ -9,31 +9,40 @@ $(document).ready(function(){
     const humidity = $(".humidity");
     const windSpeed = $(".windSpeed");
     const uvIndex = $(".uvIndex");
-
-
-});
-
-fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "units=metric&appid=" + apiKey);
-    .then((response) => response.JSON());
-    .then((data) => this.displayWeather(data));
-    
-
-displayWeather= function(data) {
-    document.querySelector(".city").innerText = "Weather in " + name; 
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°F";
-    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText = "windSpeed: " + speed + "mph";
-};
-
-
-document.querySelector(".search-button").addEventListener("click", function () {
-    weather.search();
-});
-
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-        weather.search();
+  
+    //search function
+    function search() {
+      const city = document.querySelector("#inputCity").value;
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      )
+        .then(response => response.json())
+        .then(data => displayWeather(data));
     }
-});
+  
+    //display weather function
+    function displayWeather(data) {
+      mainCity.text("City: " + data.name);
+      temp.text("Temperature: " + data.main.temp + "°F");
+      humidity.text("Humidity: " + data.main.humidity + "%");
+      windSpeed.text("Wind Speed: " + data.wind.speed + "mph");
+    }
+  
+    //event listener for search button
+    document.querySelector("#searchBtn").addEventListener("click", function() {
+      search();
+    });
+  
+    //event listener for enter key press
+    document.querySelector("#inputCity").addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+        search();
+      }
+    });
+  
+    //event listener for clear button
+    document.querySelector(".clearBtn").addEventListener("click", function() {
+      listofCities.empty();
+    });
+  });
+  
